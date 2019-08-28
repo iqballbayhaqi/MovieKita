81610f08679a0c9e069bb3ab0198c8af51282010
Iqbal baihaqi <baihaqiiqbal323@gmail.com>
2019-08-28 14:03:25 +0700

    Updates

---
+++ precache-manifest.a719c682c559b1f2856183947cd6b181.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658dfef588751686d2bfa76da55d5a9d",
+    "url": "/MovieKita/index.html"
+  },
+  {
+    "revision": "f05faf6de0f3d4d7b3ad",
+    "url": "/MovieKita/static/css/2.8d1ca2ea.chunk.css"
+  },
+  {
+    "revision": "7037be0cd3dd44eac07f",
+    "url": "/MovieKita/static/css/main.0ca18155.chunk.css"
+  },
+  {
+    "revision": "f05faf6de0f3d4d7b3ad",
+    "url": "/MovieKita/static/js/2.c83132be.chunk.js"
+  },
+  {
+    "revision": "7037be0cd3dd44eac07f",
+    "url": "/MovieKita/static/js/main.e5b34fe5.chunk.js"
+  },
+  {
+    "revision": "e1c24121fbeab30b97a4",
+    "url": "/MovieKita/static/js/runtime~main.c6ed320a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MovieKita/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MovieKita/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MovieKita/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MovieKita/static/media/slick.f97e3bbf.svg"
+  }
+]);
